d763b57878b2bd8952a012ab66a6a9649ba133ba
unknown <astrosuka@proton.me>
2022-08-05 20:39:40 -0300

    volver al card viejo porque lpm

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -1,16 +1,48 @@
-import { NFTPreview, MediaConfiguration } from "@zoralabs/nft-components";
+import { NFTPreview, NFTFullPage, MediaConfiguration } from "@zoralabs/nft-components";
 import { Networks, Strategies } from "@zoralabs/nft-hooks"
 import { CreateAsk } from "./Asks/CreateAsk";
 import AskWrite_disclosure from "./Asks/AskWrite_disclosure";
 import Image from "next/image";
 import Link from 'next/link';
-
+import { useAccount, useEnsName } from 'wagmi';
 
 const zdkStrategyMainnet = new Strategies.ZDKFetchStrategy(
     Networks.MAINNET
 )
 
+const STYLE_OVERRIDE = {
+    theme: {
+      bodyFont: `
+              font-family: courier;
+          `,
+        titleFont: `font-family: courier;`,
+    borderStyle: "dashed 1px",
+    defaultBorderRadius: 0,
+    preferredIPFSGateway: "https://ipfs.io/ipfs/",
+    showOwner: false,
+    showCreator: false,
+      previewCard: {
+        background: '#f5f5f5'
+        // height: "20vh",
+      },
+
+    padding: '3px',
+
+    showTxnLinks: true,
+    useEnsResolution: true,
+    useCollectionTag: true,
+    // textBlockPadding: "2px",
+    
+
+
+    },
+  };
+
 const NFTCard = ({ nfts }) => {
+
+
+
+
 
     return (
         <>
@@ -18,37 +50,66 @@ const NFTCard = ({ nfts }) => {
             nfts && nfts.length > 0
             ?
             nfts.map((nft, index) => {
-                console.log("que es")
+
+
+                // const ensHelper = (addressINGTON) => {
+                //     if (!!nft.token.owner) {
+                //     const { data, isError, isLoading } = useEnsName({
+                //       address: addressINGTON,
+                //     })
+                //     const renderedAddress = !!data ? data : addressINGTON
+                //     return renderedAddress
+                //   } else {
+                //     return addressINGTON
+                //   }}
+
                 return (
-                    <div key={nft.token.tokenId} className="border border-black p-3 m-2 w-10/10 sm:w-5/5 md:w-3/3 border-dashed flex flex-col flex-wrap justify-center">
+                 
+                    <div key={nft.token.tokenId} className="w-10/10 sm:w-5/5 md:w-3/3 flex flex-col flex-wrap justify-center">
                        
-                        {/* <MediaConfiguration
+                        <MediaConfiguration
+                        style={STYLE_OVERRIDE}
                         networkId="1"                        
                         strategy={zdkStrategyMainnet}
                         strings={{
                             CARD_OWNED_BY: "↳",
-                            CARD_CREATED_BY: "↳",                           
-                        }}                    
+                            CARD_CREATED_BY: "↳",
+                                                     
+                        }}   
+                                         
                         >
                         <NFTPreview
+                            
                             href={`https://zora.co/collections/0x532f7DB02D2ebE12f2CDdfAcDa807FD9B2D96F66/${nft.token.tokenId}`}
                             contract={nft.token.collectionAddress}
                             id={nft.token.tokenId}
                             showBids={false}
-                            showPerpetual={false}                           
+                            showPerpetual={false} 
+                                              
                         />
-                        </MediaConfiguration> */}
-                            <Link href={`https://ipfs.io/ipfs/${nft.token.metadata.image.slice(7)}`}><a target="_blank" rel="noreferrer"><Image
+                        </MediaConfiguration>
+
+                        {/* <Link href={`https://ipfs.io/ipfs/${nft.token.metadata.image.slice(7)}`}><a target="_blank" rel="noreferrer">view on ipfs</a></Link> */}
+                            {/* <Link href={`https://ipfs.io/ipfs/${nft.token.metadata.image.slice(7)}`}><a target="_blank" rel="noreferrer"><Image
                                 src={`https://ipfs.io/ipfs/${nft.token.metadata.image.slice(7)}`}
                                 width={300}
                                 height={300}
-                            /></a></Link>
+                            /></a></Link> */}
                             
-                            <div>
-
-                            {/* ↳owner{`${nft.token.owner}`} */}
+                            {/* <div>
+                            <div className=""> 
+        {"ens name is " + ensHelper(nft.token.owner)}
+      </div>
+                            ↳owner{`${nft.token.owner}`}
                             <p className='text-sm'>
                             ↳owner:<span> </span>
+
+                            { ensHelper(nft.token.owner) == nft.token.owner ? (
+                                <Link href={`https://etherscan.io/address/${nft.token.owner}`}><a target="_blank" rel="noreferrer">{`${nft.token.owner.slice(-42, -37)}`}...{`${nft.token.owner.slice(38)}`}</a>
+                                </Link>
+                            ) : (<Link href={`https://etherscan.io/address/${nft.token.owner}`}><a target="_blank" rel="noreferrer">{ensHelper(nft.token.owner)}</a>
+                            </Link>)}
+                            
                             <Link href={`https://etherscan.io/address/${nft.token.owner}`}><a target="_blank" rel="noreferrer">{`${nft.token.owner.slice(-42, -37)}`}...{`${nft.token.owner.slice(38)}`}</a>
                             </Link>
                             </p>
@@ -59,7 +120,7 @@ const NFTCard = ({ nfts }) => {
                             <Link href={`https://opensea.io/assets/ethereum/${nft.token.collectionAddress}/${nft.token.tokenId}`}><a target="_blank" rel="noreferrer">opensea</a>
                             </Link>
                             </p>
-                            </div>
+                            </div> */}
                         
                         {/* <audio
                             controls
