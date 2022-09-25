3ba0a2372f68c6193b4e5a3bcb0cbf22969c2a3e
sofffja <sofi.efron@gmail.com>
2022-09-25 16:12:34 -0300

    margencito about

--- pages/about.tsx
+++ pages/about.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <meta name="description" content="✧unun." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col m-[10%] md:m-[25%] mb-20">        
+      <main className="flex flex-col m-[10%] md:m-[30%] mb-20">        
         <h1 className="text-3xl mb-3 drop-shadow-hard text-[#f5f5f5]">
           {'✧unun.'}
         </h1>
