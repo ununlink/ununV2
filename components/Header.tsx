4c8a4875748516d8bb4d74d48b70bbc1b8a4902f
sofffja <sofi.efron@gmail.com>
2022-09-27 12:02:07 -0300

    create

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import { ElBoton } from "./CustomButton";
 export const Header = () => {
 
   return (
-    <div className="p-2 z-10 fixed top-0 left-0 right-0 flex justify-between backdrop-blur-md">
+    <div className="p-2 z-10 fixed top-0 left-0 right-0 flex justify-between backdrop-blur-md menu">
 
       <div className="flex flex-row w-fit space-x-4">                   
         <Link
