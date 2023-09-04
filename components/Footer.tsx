a69d6d7f63e74c84be2d68ed5d2f45ef02cf39c2
astrosuka <yegresvoselok@gmail.com>
2023-09-04 17:38:30 -0300

    bordecito

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
 
   return (
    <div className="flex flex-col items-center z-10 fixed bottom-0 left-0 right-0 flex justify-between backdrop-blur-md">
-      <div className='w-[100%]'>
+      <div className='w-[100%] border-t'>
           <NFTPlayer />
         </div>
    </div>
