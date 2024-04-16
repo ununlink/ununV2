d081ee0c65a2652878c7b67556c50d3318da741d
astrosuka <yegresvoselok@gmail.com>
2024-04-16 00:00:06 -0300

    Create next

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>UNDER CONSTRUCTION</h1>
+      <div class="links">
+        <a href="https://unun.bandcamp.com">bandcamp</a>
+        <a href="https://unun.bandcamp.com">soundcloud</a>
+        <a href="https://unun.bandcamp.com">nina</a>
+        <a href="https://unun.bandcamp.com">instagram</a>
+        <a href="https://unun.bandcamp.com">twitter</a>
+      </div>
+    </main>
+  );
+}
