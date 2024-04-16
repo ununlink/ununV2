f238cdb261c895b6e445762cd3f3e3ca0d8fbf3e
astrosuka <yegresvoselok@gmail.com>
2024-04-15 23:33:47 -0300

    Make it very simple

--- constants/Constants.tsx
+++
@@ -1,8 +0,0 @@
-const ourCollection = '0x532f7DB02D2ebE12f2CDdfAcDa807FD9B2D96F66'
-const ourCollection2 = '0x74345098d3c86571fef44f0aab097f34c354676e'
-
-const tokenPrice = 0.02
-const tokenPrice2 = 0.03
-
-
-export {ourCollection, ourCollection2, tokenPrice, tokenPrice2}
